ad55f912829dc92d1a273a0b4ef4cc7ad105274a
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-06 21:00:43 +0530

    chore: Update dependencies and add next-auth package
    Add NEXTAUTH_SECRET to .env file
    Update .gitignore to include next-env.d.ts
    Add sign-in page component
    Update auth options in options.ts
    Add route handler for next-auth
    Add AuthProvider component
    Add middleware for authentication
    Add type declarations for next-auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
+
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
@@ -44,34 +45,29 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-
     async jwt({ token, user }) {
-
       if (user) {
         token._id = user._id?.toString(); // Convert ObjectId to string
         token.isVerified = user.isVerified;
         token.isAcceptingMessages = user.isAcceptingMessages;
         token.username = user.username;
       }
-      
       return token;
     },
     async session({ session, token }) {
-
       if (token) {
         session.user._id = token._id;
         session.user.isVerified = token.isVerified;
         session.user.isAcceptingMessages = token.isAcceptingMessages;
         session.user.username = token.username;
       }
-
       return session;
     },
   },
   session: {
     strategy: "jwt",
   },
-  secret: process.env.NEXTAUTH_SECRET, 
+  secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/sign-in",
   },
